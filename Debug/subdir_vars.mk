54619349252bb77611081b395eeacabf1ab9d575
Jocarsoli2001 <Jocarsoli2001@gmail.com>
2022-06-02 12:19:29 -0600

    Proyecto final - terminado
    Se logró generar la página web con el control de todos los parqueos. También se pueden controlar los números de parqueos disponibles desde la página web.

--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -8,9 +8,6 @@ SHELL = cmd.exe
 CMD_SRCS += \
 ../tm4c123gh6pm.cmd 
 
-INO_SRCS += \
-../WebServer.ino 
-
 LIB_SRCS += \
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/ccs/Debug/driverlib.lib 
 
@@ -18,32 +15,21 @@ C_SRCS += \
 ../main.c \
 ../tm4c123gh6pm_startup_ccs.c 
 
-INO_DEPS += \
-./WebServer.d 
-
 C_DEPS += \
 ./main.d \
 ./tm4c123gh6pm_startup_ccs.d 
 
 OBJS += \
-./WebServer.obj \
 ./main.obj \
 ./tm4c123gh6pm_startup_ccs.obj 
 
 OBJS__QUOTED += \
-"WebServer.obj" \
 "main.obj" \
 "tm4c123gh6pm_startup_ccs.obj" 
-
-INO_DEPS__QUOTED += \
-"WebServer.d" 
 
 C_DEPS__QUOTED += \
 "main.d" \
 "tm4c123gh6pm_startup_ccs.d" 
-
-INO_SRCS__QUOTED += \
-"../WebServer.ino" 
 
 C_SRCS__QUOTED += \
 "../main.c" \
